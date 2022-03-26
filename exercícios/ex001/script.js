976f37a3a8f0a382801807857ff536b98feb86bc
Hugo Cicillini <hugo.cicillini@hotmail.com>
2022-03-26 17:32:24 -0300

    .

---
+++ exercícios/ex001/script.js
@@ -0,0 +1,8 @@
+<script>
+        function myFunction () {
+            document.getElementById('demo1').innerHTML = "Trouxa"
+        }
+        function myFunction2 () {
+            document.getElementById('demo2').innerHTML = "Trouxa 2x"
+        }
+    </script>
